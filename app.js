9f264c31967b487d2914f8408e37b4ed412622dc
cvipera <delaporte@et.esiea.fr>
2024-11-12 00:48:12 +0100

    the second world

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World from branch 2!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Server running on http://0.0.0.0:3000");
 });
